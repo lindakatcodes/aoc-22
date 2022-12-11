9d4ab998a1db0e1fb6fdd7f325dbdd1b2abdc1a1
LindaKat <lindakt16@gmail.com>
2022-12-10 20:50:59 -0600

    day 9 part 1 and start at part 2

--- solutions/day09.js
+++ solutions/day09.js
@@ -6,19 +6,120 @@ const readData = async function (filePath) {
 const data = await readData("./inputs/day09input.txt");
 const moves = data.split("\n");
 
-// just need to keep track of x and y numbers; set to store the visit coords, so the final count will be the unique values
-// h and t have to touch - either up/down/left/right or diagonal; tail has to be within 2 spots of h; t moves up/down/left/right if the positions touch - if diagonal, move is diagonal towards the h (so ends up a main direction)
+// part 1
+// let hx = 0;
+// let hy = 0;
+// let tx = 0;
+// let ty = 0;
 
-// x is l/r, y is u/d - x always first
+// const tailVisits = new Set();
 
-let hx = 0;
-let hy = 0;
-let tx = 0;
-let ty = 0;
+// tailVisits.add(`${tx} ${ty}`);
 
-const tailVisits = new Set();
+// for (let i = 0; i < moves.length; i++) {
+//   const split = moves[i].split(" ");
+//   const direction = split[0];
+//   const steps = +split[1];
 
-tailVisits.add(`${tx} ${ty}`);
+//   for (let j = 0; j < steps; j++) {
+//     // move h
+//     switch (direction) {
+//       case "U":
+//         hy++;
+//         break;
+//       case "D":
+//         hy--;
+//         break;
+//       case "L":
+//         hx--;
+//         break;
+//       case "R":
+//         hx++;
+//         break;
+//     }
+
+//     // sideways if x or y are the same
+//     if (hx === tx || hy === ty) {
+//       if (hx === tx && Math.abs(hy - ty) === 2) {
+//         direction === "U" ? ty++ : ty--;
+//       }
+//       if (hy === ty && Math.abs(hx - tx) === 2) {
+//         direction === "R" ? tx++ : tx--;
+//       }
+//     } else {
+//       // otherwise diagonal
+//       const xdiff = Math.abs(hx - tx);
+//       const ydiff = Math.abs(hy - ty);
+//       if (xdiff === 2 || ydiff === 2) {
+//         switch (direction) {
+//           case "U":
+//             ty = hy - 1;
+//             tx = hx;
+//             break;
+//           case "D":
+//             ty = hy + 1;
+//             tx = hx;
+//             break;
+//           case "L":
+//             ty = hy;
+//             tx = hx + 1;
+//             break;
+//           case "R":
+//             ty = hy;
+//             tx = hx - 1;
+//             break;
+//         }
+//       }
+//     }
+//     tailVisits.add(`${tx} ${ty}`);
+//   }
+// }
+
+// console.log(tailVisits);
+// console.log(tailVisits.size);
+
+// part 2 
+const xvals = [0,0,0,0,0,0,0,0,0,0];
+const yvals = [0,0,0,0,0,0,0,0,0,0];
+const tailVals = new Set();
+tailVals.add(`0 0`);
+
+function getNewLocation(hx, hy, tx, ty, direction) {
+  // sideways if x or y are the same
+  if (hx === tx || hy === ty) {
+    if (hx === tx && Math.abs(hy - ty) === 2) {
+      direction === "U" ? ty++ : ty--;
+    }
+    if (hy === ty && Math.abs(hx - tx) === 2) {
+      direction === "R" ? tx++ : tx--;
+    }
+  } else {
+    // otherwise diagonal
+    const xdiff = Math.abs(hx - tx);
+    const ydiff = Math.abs(hy - ty);
+    if (xdiff === 2 || ydiff === 2) {
+      switch (direction) {
+        case "U":
+          ty = hy - 1;
+          tx = hx;
+          break;
+        case "D":
+          ty = hy + 1;
+          tx = hx;
+          break;
+        case "L":
+          ty = hy;
+          tx = hx + 1;
+          break;
+        case "R":
+          ty = hy;
+          tx = hx - 1;
+          break;
+      }
+    }
+  }
+  return [tx, ty];
+}
 
 for (let i = 0; i < moves.length; i++) {
   const split = moves[i].split(" ");
@@ -29,55 +130,26 @@ for (let i = 0; i < moves.length; i++) {
     // move h
     switch (direction) {
       case "U":
-        hy++;
+        yvals[0] = yvals[0] + 1;
         break;
       case "D":
-        hy--;
+        yvals[0] = yvals[0] - 1;
         break;
       case "L":
-        hx--;
+        xvals[0] = xvals[0] - 1;
         break;
       case "R":
-        hx++;
+        xvals[0] = xvals[0] + 1;
         break;
     }
 
-    // sideways if x or y are the same
-    if (hx === tx || hy === ty) {
-      if (hx === tx && Math.abs(hy - ty) === 2) {
-        direction === "U" ? ty++ : ty--;
-      }
-      if (hy === ty && Math.abs(hx - tx) === 2) {
-        direction === "R" ? tx++ : tx--;
-      }
-    } else {
-      // otherwise diagonal
-      const xdiff = Math.abs(hx - tx);
-      const ydiff = Math.abs(hy - ty);
-      if (xdiff === 2 || ydiff === 2) {
-        switch (direction) {
-          case "U":
-            ty = hy - 1;
-            tx = hx;
-            break;
-          case "D":
-            ty = hy + 1;
-            tx = hx;
-            break;
-          case "L":
-            ty = hy;
-            tx = hx + 1;
-            break;
-          case "R":
-            ty = hy;
-            tx = hx - 1;
-            break;
-        }
-      }
+    for (let a = 0; a < xvals.length - 1; a++) {
+      const updates = getNewLocation(xvals[a], yvals[a], xvals[a + 1], yvals[a + 1], direction);
+      xvals[a + 1] = updates[0];
+      yvals[a + 1] = updates[1];
     }
-    tailVisits.add(`${tx} ${ty}`);
+  tailVals.add(`${xvals[9]} ${yvals[9]}`);
   }
 }
 
-// console.log(tailVisits);
-console.log(tailVisits.size);
+console.log(tailVals)
